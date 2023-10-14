1e39fd90b667a02a385df435229694205abb4306
Hindol Roy Choudhury <67415065+Hindolch@users.noreply.github.com>
2023-10-14 21:43:35 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import pickle
+import string
+from nltk.corpus import stopwords
+import nltk
+from nltk.stem.porter import PorterStemmer
+
+ps = PorterStemmer()
+
+
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
+
+    return " ".join(y)
+
+cv = pickle.load(open('countVec.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
+
+st.title("Sentiment analysis for Amazon product review")
+
+input_review = st.text_area("Enter the message")
+
+if st.button('Predict'):
+
+    # 1. preprocess
+    transformed_review = transform_text(input_review)
+    # 2. vectorize
+    vector_input = cv.transform([transformed_review])
+    # 3. predict
+    result = model.predict(vector_input)[0]
+    # 4. Display
+    if result == 1:
+        st.header('Very negative review')
+    elif result == 2:
+        st.header('Negative review')
+    elif result == 3:
+        st.header('Neutral review')
+    elif result == 4:
+        st.header('Positive review')
+    elif result == 5:
+        st.header('Very positive review')
